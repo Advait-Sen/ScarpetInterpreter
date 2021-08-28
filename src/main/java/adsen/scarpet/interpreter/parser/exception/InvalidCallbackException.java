32fecbe5058ab3fb28734e84182784f52528f89d
Ghoulboy78 <ghoulboy27@gmail.com>
2021-08-28 12:02:15 +0200

    released version 0.0.0 to maven - test

--- src/main/java/adsen/scarpet/interpreter/parser/exception/InvalidCallbackException.java
+++
@@ -1,5 +0,0 @@
-package adsen.scarpet.interpreter.parser.exception;
-
-public class InvalidCallbackException extends Exception
-{
-}
--- src/main/java/adsen/scarpet/interpreter/parser/exception/ScarpetExpressionException.java
+++
@@ -1,9 +0,0 @@
-package adsen.scarpet.interpreter.parser.exception;
-
-public class ScarpetExpressionException extends ExpressionException
-{
-    public ScarpetExpressionException(String message)
-    {
-        super(message);
-    }
-}
